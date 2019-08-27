9a3ee26681bb02312943290a2cf53b32a6a72273
Shota Ishikawa <ishota1991@gmail.com>
2019-08-28 00:15:46 +0900

    suspended

--- P1.py
+++ P1.py
@@ -3,6 +3,7 @@
 import os
 import glob
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from PIL import Image
 import numpy as np
 import utl
@@ -11,7 +12,7 @@ import utl
 im_list = []
 images = glob.glob(os.path.join("./test_images/", "*.jpg"))
 num_images = len(images)
-fig, axs = plt.subplots(len(images), 5, figsize=(5, 8))
+fig, axs = plt.subplots(len(images), 6, figsize=(5, 8))
 for i in range(num_images):
     img = np.array(Image.open(images[i]))
     im_list.append(img)
@@ -46,15 +47,34 @@ for i in range(num_images):
 mask = np.zeros_like(edges_im_list[0])
 ignore_mask_color = 255
 im_shape = edges_im_list[0].shape
-vertices = np.array([[(30, im_shape[0]),
-                      (2.4 * im_shape[1] / 5, im_shape[0] / 2),
-                      (2.6 * im_shape[1] / 5, im_shape[0] / 2),
-                      (im_shape[1] - 30, im_shape[0])]],
+vertices = np.array([[(30, im_shape[0]), (2.4 * im_shape[1] / 5, im_shape[0] / 2),
+                      (2.6 * im_shape[1] / 5, im_shape[0] / 2), (im_shape[1] - 30, im_shape[0])]],
                     dtype=np.int32)
 masked_im_list = []
 for i in range(num_images):
     masked_im_list.append(utl.region_of_interest(edges_im_list[i], vertices))
     axs[i, 4].imshow(masked_im_list[i], cmap='gray')
     axs[i, 4].axis("off")
+
+# Define the Hough transform parameters
+# Make a blank the same size as our image to draw on
+rho = 1
+theta = np.pi/180
+threshold = 65
+min_line_length = 30
+max_line_gap = 2
+
+hough_im_list = []
+blank_im_list = []
+for i in range(num_images):
+    blank_im_list.append(np.copy(masked_im_list[i])*0)
+    lines = utl.hough_lines(masked_im_list[i], rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+
+    for line in lines:
+        utl.draw_lines(blank_im_list[i], lines, (255, 0, 0), 10)
+
+    hough_im_list.append(np.dstack((edges_im_list[i], edges_im_list[i], edges_im_list[i])))
+    axs[i, 5].imshow(hough_im_list[i])
+    axs[i, 5].axis("off")
 
 plt.show()
